c090e62eb21dec7a129c37d716a8284f5153b438
kanarne <kanarne@gmail.com>
2018-11-04 23:10:42 +0900

    setup shell

--- setup.sh
+++ setup.sh
@@ -6,47 +6,19 @@ os_type() {
 }
 
 symlink() {
-  for f in .??*
-  do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
+  for f in $( ls . ); do
+    [[ "$f" == "setup.sh" ]] && continue
     if [ "$(os_type)" = "Linux" ]; then
-      [[ "$f" == ".tmux.conf" ]] && continue
+      [[ "$f" == "tmux.conf" ]] && continue
     fi
-    ln -s -f $HOME/dotfiles/$f  $HOME/$f
+    ln -s -f $HOME/dotfiles/$f  $HOME/.$f
   done
-}
-
-pyenv_download() {
-  if [ ! -d $HOME/.pyenv ]; then
-    git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
-    git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
-  fi
-}
-
-zplug_download() {
-  if [ ! -d $HOME/.zplug ]; then
-    git clone https://github.com/zplug/zplug $HOME/.zplug
-  fi
-}
-
-vimplug_download() {
-  if [ ! -d $HOME/.vim ]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  fi
 }
 
 if [ "$(os_type)" = "Darwin" ]; then
   symlink
-  pyenv_download
-  zplug_download
-  vimplug_download
 elif [ "$(os_type)" = "Linux" ]; then
   symlink
-  pyenv_download
-  zplug_download
-  vimplug_download
 else
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
